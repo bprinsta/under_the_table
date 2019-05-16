802bd8d7bef80a7f0319d9e39f7e26024a770d43
Ben Musoke-Lubega <benmusokelubega@princeton.edu>
2019-05-16 17:00:15 -0400

    added second wave of police

--- js/boot.js
+++ js/boot.js
@@ -14,7 +14,8 @@ var BootScene = new Phaser.Class({
         
         // map in json format
         //this.load.tilemapTiledJSON('map', 'assets/map/map.json');
-        this.load.tilemapTiledJSON('map', 'assets/map/level0.json');
+        this.load.tilemapTiledJSON('map1', 'assets/map/level1.json');
+        this.load.tilemapTiledJSON('map0', 'assets/map/level0.json');
         
         // our two characters
         this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
@@ -26,16 +27,17 @@ var BootScene = new Phaser.Class({
         // load portal
         this.load.image('portal', 'assets/portal_small.png');
 
-       // this.load.audio('music', 'assets/Battle Theme II v1.2.wav');
+        this.load.audio('music', 'assets/Battle Theme II v1.2.wav');
         // this.load.audio('alert', 'assets/pokemon.wav');
     },
 
     create: function ()
     {
-       // this.sound.add('music');
-        // sthis.sound.add('alert');
-        // this.sound.play('music');
+       this.sound.add('music');
+        // this.sound.add('alert');
+        this.sound.play('music');
         // this.sound.once('complete', this.gameOver);
+        this.level = 0;
 
         // start the WorldScene
         this.scene.start('WorldScene');
--- js/game.js
+++ js/game.js
@@ -73,16 +73,22 @@ var WorldScene = new Phaser.Class({
 
     create: function ()
     {
-        // create the map
-        var map = this.add.tilemap('map');
+        console.log(this.level);
+        
+        var map;
+        //if (this.level == 0) 
+        {
+            map = this.add.tilemap('map0'); 
+        }
+        // else 
+        {
+         //   map = this.add.tilemap('map1');
+        }
 
         // first parameter is the name of the tilemap in tiled
-        // var tiles = map.addTilesetImage('spritesheet', 'tiles');
         var tiles = map.addTilesetImage('modern', 'tiles');
         
         // creating the layers
-        //var grass = map.createStaticLayer('Grass', tiles, 0, 0);
-        //var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
         var ground = map.createStaticLayer('Below Player', tiles, 0, 0);
         var obstacles = map.createStaticLayer('World Level', tiles, 0, 0);
         var aboveLayer = map.createStaticLayer('Above Player', tiles, 0, 0);
@@ -94,7 +100,7 @@ var WorldScene = new Phaser.Class({
 
         // set player animation frames
         this.playerAnimations();
-        this.playerSpeed = 96;
+        this.playerSpeed = 80;
 
         const spawnPoint = map.findObject("Objects", obj => obj.name === "Player Spawn");
         const endPoint = map.findObject("Objects", obj => obj.name === "Portal");
@@ -141,16 +147,33 @@ var WorldScene = new Phaser.Class({
                 stepY: 4 * PIXEL_SIZE
             }
         })
+
+        this.enemies2 = this.physics.add.group({
+            key: 'officer', 
+            repeat: 2,
+            setXY: {
+                x: 5 * PIXEL_SIZE,
+                y: 15 * PIXEL_SIZE,
+                stepX: 5 * PIXEL_SIZE
+            }
+        })
+
         var police_points = []
         this.policeAnimations();
 
+        Phaser.Actions.ScaleXY(this.enemies2.getChildren(), -0.5, -0.5);
         Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
         
         // where the enemies will be
         this.spawns = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
+        this.spawns2 = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
        
         this.enemies.getChildren().forEach(element => {
             this.spawns.create(element.x - 32, element.y, 48, 16); 
+        });
+
+        this.enemies2.getChildren().forEach(element => {
+            this.spawns2.create(element.x, element.y - 30, 16, 32); 
         });
 
         var tween = this.tweens.add({
@@ -173,14 +196,37 @@ var WorldScene = new Phaser.Class({
             yoyo: true
         });
 
+        this.tweens.add({
+            targets: this.enemies2.getChildren(),
+            y: '-=48',
+            // x: '+= 80',
+            ease: 'Linear',
+            duration: 1600,
+            repeat: -1,
+            yoyo: true
+        });
+
+        this.tweens.add({
+            targets: this.spawns2.getChildren(),
+            y: '-=48',
+            // x: '+= 80',
+            ease: 'Linear',
+            duration: 1600,
+            //delay: 500,
+            repeat: -1,
+            yoyo: true
+        });
+
         this.enemies.getChildren().forEach(element => {
             element.anims.play('o_down', true);
         });
 
         // add collider
         this.physics.add.overlap(this.player, this.enemies, this.gameOver, false, this);
+        this.physics.add.overlap(this.player, this.enemies2, this.gameOver, false, this);
 
         this.physics.add.overlap(this.player, this.spawns, this.gameOver, false, this);
+        this.physics.add.overlap(this.player, this.spawns2, this.gameOver, false, this);
 
         this.physics.add.overlap(this.player, this.portal, this.gameWin, false, this);
     },
@@ -259,6 +305,31 @@ var WorldScene = new Phaser.Class({
                     element.anims.stop();
             }
         });
+
+        this.enemies2.getChildren().forEach(element => {
+            if (element.body.velocity.x < 0)
+            {
+                    element.anims.play('o_left', true);
+                    element.flipX = true;
+            }
+            else if (element.body.velocity.x > 0)
+            {
+                    element.anims.play('o_right', true);
+                    element.flipX = true;
+            }
+            else if (element.body.velocity.y < 0)
+            {
+                    element.anims.play('o_up', true);
+            }
+            else if (element.body.velocity.y > 0)
+            {
+                    element.anims.play('o_down', true);
+            }
+            else
+            {
+                    element.anims.stop();
+            }
+        });
     },
     update: function (time, delta)
     {
@@ -268,6 +339,9 @@ var WorldScene = new Phaser.Class({
 
         this.enemies.getChildren().forEach(element => {
             element.body.setVelocityX(16);
+        });
+        this.enemies2.getChildren().forEach(element => {
+            element.body.setVelocityY(-1);
         });
        //  console.log(this.enemies.getChildren())
     },
@@ -296,6 +370,7 @@ var WorldScene = new Phaser.Class({
 
         // shake the world
         // this.cameras.main.shake(300);
+        this.level = 1;
         
         // fade camera
         this.time.delayedCall(250, function() {
@@ -304,7 +379,7 @@ var WorldScene = new Phaser.Class({
 
         // restart game
         this.time.delayedCall(500, function() {
-            this.scene.restart();
+            this.scene.start('WorldScene');
         }, [], this);
     }
 });
@@ -323,7 +398,7 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: false // set to true to view zones
+            debug: true // set to true to view zones
         }
     },
     scene: [
